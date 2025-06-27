34edc7021e73563215ac62a51e55cc6118f1af1a
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2025-06-27 23:24:59 +0200

    chore: updating to tailwind 4, not working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,44 +1,48 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-    site: 'https://manuelarte.github.io',
-    output: 'static',
-    trailingSlash: 'always',
+  site: 'https://manuelarte.github.io',
+  output: 'static',
+  trailingSlash: 'always',
 
-    // Single page, no prefetch needed
-    prefetch: false,
+  // Single page, no prefetch needed
+  prefetch: false,
 
-    integrations: [
-        tailwind(),
-        sitemap(),
-        compress({
-            CSS: true,
-            SVG: false,
-            Image: false,
-            HTML: {
-                "html-minifier-terser": {
-                    collapseWhitespace: true,
-                    // collapseInlineTagWhitespace: true, // It breaks display-inline / flex-inline text
-                    minifyCSS: true,
-                    minifyJS: true,
-                    removeComments: true,
-                    removeEmptyAttributes: true,
-                    // removeEmptyElements: true, // It removes sometimes SVGs
-                    removeRedundantAttributes: true
-                },
-            },
-            JavaScript: {
-                'terser': {
-                    compress: {
-                        drop_console: true,
-                        drop_debugger: true,
-                    }
-                }
-            }
-        })
-    ]
+  integrations: [
+      sitemap(),
+      compress({
+          CSS: true,
+          SVG: false,
+          Image: false,
+          HTML: {
+              "html-minifier-terser": {
+                  collapseWhitespace: true,
+                  // collapseInlineTagWhitespace: true, // It breaks display-inline / flex-inline text
+                  minifyCSS: true,
+                  minifyJS: true,
+                  removeComments: true,
+                  removeEmptyAttributes: true,
+                  // removeEmptyElements: true, // It removes sometimes SVGs
+                  removeRedundantAttributes: true
+              },
+          },
+          JavaScript: {
+              'terser': {
+                  compress: {
+                      drop_console: true,
+                      drop_debugger: true,
+                  }
+              }
+          }
+      })
+  ],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
